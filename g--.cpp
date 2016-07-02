3f8474f533a6b550b992ceac88c1404b791ca861
Tropic <tropical.productions.scratch@gmail.com>
2016-07-02 23:49:34 +0200

    Update and rename compiler.cpp to g--.cpp

--- g--.cpp
+++ g--.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	if(argc < 2) {
-		cout << "Please specify the file you want to compile.";
+		cerr << "Usage: " + argv[0] + " <file_name>";
+		return 1;
 	} else {
 		cout << "Reading file...";
 		return 0;
