e696757bac6ec47457a38c3c9091455dafe18efc
TropicSapling <tropical.productions.scratch@gmail.com>
2016-07-03 21:37:56 +0200

    File reading now working

--- g--.cpp
+++ g--.cpp
@@ -7,16 +7,21 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	if(argc < 2) {
-		cerr << "Usage: " + argv[0] + " <file_name>";
+		cerr << string("Usage: ") + string(argv[0]) + string(" <file_name>\n");
 		return 1;
 	} else {
-		cout << "Reading file...";
-		
+		cout << "Reading file...\n";
 		ifstream file(argv[1]);
-    		string line;
-    		while (getline(file, line)) {
-        		// Process line
-    		}
+		if(file.good()) {
+			string line;
+			while (getline(file, line)) {
+				// Process line
+				cout << line << endl;
+			}
+		} else {
+			cerr << "[!!] ERROR: Failed to read file. Perhaps you specified the wrong file name?\n";
+			return 1;
+		}
 		
 		return 0;
 	}
