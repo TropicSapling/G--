b5fce1d232ad58320d4f454693271ef600469d32
Tropic <tropical.productions.scratch@gmail.com>
2016-07-12 16:21:34 +0200

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 int main () {
    double* pvalue = NULL; // Pointer initialized with null
-   pvalue = new double; // Request memory for the variable
+   pvalue = new double; // Request memory for the variable, g-- registers the pointer
  
    *pvalue = 29494.99; // Store value at allocated address
-   cout << "Value of pvalue : " << *pvalue << endl;
-
-   delete pvalue; // Free up the memory, this is added by the garbage collector
+   cout << "Value of pvalue : " << *pvalue << endl; // Prints the value, g-- detects the latest use of the pointer here
+   delete pvalue; // Therefore g-- deletes the pointer right after, freeing the memory
 
    return 0;
 }
