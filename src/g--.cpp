9a40764a118b4685118025861779ed53842707be
Tropic <tropical.productions.scratch@gmail.com>
2016-07-15 20:06:49 +0200

    Bugfixes and removed old code

--- src/g--.cpp
+++ src/g--.cpp
@@ -64,18 +64,18 @@ int main(int argc, char* argv[]) {
 
 					unsigned short int j = i;
 					
-					while(file_contents.at(j) != "=") {
+					while(string(1, file_contents.at(j)) != "=") {
 						j--;
 					}
 					j--;
 					
-					while(file_contents.at(j) == " ") {
+					while(string(1, file_contents.at(j)) == " ") {
 							j--;
 					}
 					
-					string obj = file_contents.at(j);
+					string obj = string(1, file_contents.at(j));
 					j--;
-					while(string(1, file_contents.at(j)) != " " && string(1, file_contents.at(j)) != ";" && j > 0) {
+					while(string(1, file_contents.at(j)) != " " && string(1, file_contents.at(j)) != ";" && j >= 0) {
 						obj += string(1, file_contents.at(j));
 						j--;
 					}
@@ -92,48 +92,8 @@ int main(int argc, char* argv[]) {
 				} else if(hasEnding(file_contents, "delete")) {
 					
 				}
-				
-/*				if(line.find("new")) {
-					size_t ch = line.find("new");
 					
-					if(ch != string::npos) {
-						string isArray = "false";
-						
-						if(line.find("[]") != string::npos) {
-							isArray = "true";
-						}
-						
-						while(string(1, line.at(ch)) != "=") {
-							ch--;
-						}
-						ch--;
-						
-						while(string(1, line.at(ch)) == " ") {
-							ch--;
-						}
-						
-						string obj = string(1, line.at(ch));
-						ch--;
-						while(string(1, line.at(ch)) != " " && string(1, line.at(ch)) != ";" && ch > 0) {
-							obj += string(1, line.at(ch));
-							ch--;
-						}
-						
-						reverse(obj.begin(), obj.end());
-						objects.push_back({obj, isArray, string(i)});
-						
-						if(debugMode) {
-							cout << "[DEBUG] obj: ";
-							cout << obj << ", isArray: ";
-							cout << isArray << ", line: ";
-							cout << i << "\n";
-						}
-					}
-				} else if(line.find("delete") {
-					// WIP
-				}
-					
-				for(unsigned j = 0; j < objects.size(); j++) {
+/*				for(unsigned j = 0; j < objects.size(); j++) {
 					if(line.find(objects[j][0])) {
 						objects[j][1] = i;
 					}
