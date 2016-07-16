3a36f082eb6a422cabed5f1e9bebd9af73e3d6b4
Tropic <tropical.productions.scratch@gmail.com>
2016-07-16 13:42:10 +0200

    Reinabled io syncing because of problems

--- src/g--.cpp
+++ src/g--.cpp
@@ -6,8 +6,6 @@
 
 using namespace std;
 
-ios_base::sync_with_stdio(false);
-
 bool hasEnding (std::string const &fullString, std::string const &ending) {
     if (fullString.length() >= ending.length()) {
         return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
